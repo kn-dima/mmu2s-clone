964e55fe0a658c382b4076050894d262aed4a31a
kn-dima <kn-dima@yandex.ru>
2020-04-12 00:49:36 +0400

    keiboard input processing moved to function

--- mmu2-diy/application.h
+++ mmu2-diy/application.h
@@ -8,6 +8,7 @@ extern bool isFilamentLoadedtoExtruder();
 
 extern void initIdlerPosition();
 extern void checkSerialInterface();
+extern void checkDebugSerialInterface();
 extern String ReadSerialStrUntilNewLine();
 extern void initColorSelector();
 extern void filamentLoadToMK3();
